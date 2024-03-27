2b54c34caa9403eaf2cfa555bb45b25e35c5a909
OnichanSaskeh <165002705+OnichanSaskeh@users.noreply.github.com>
2024-03-27 19:10:01 +0700

    Update logger_sv.lua

--- logger_sv.lua
+++ logger_sv.lua
@@ -1,10 +1,10 @@
-local logs = "Change this to your webhook"
+local logs = "https://discord.com/api/webhooks/1222507893228699670/EfuNu_KS850Ak_sPfuIoXle-ZPsaYrd4_sEB4E8c4a9aEQIQ8aJiO8HvZWtrWgWhpFVV"
 local communityname = "Big Yoda"
-local communtiylogo = "https://i.imgur.com/e8VsdLL.jpg" --Must end with .png or .jpg
+local communtiylogo = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.etsy.com%2Fin-en%2Flisting%2F573644286%2Ftic-tac-toe-game-sheet-digital-print&psig=AOvVaw1pXcEULbkVR1TG227jk2zk&ust=1711627687216000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIiv2pO0lIUDFQAAAAAdAAAAABAE" --Must end with .png or .jpg
 
 AddEventHandler('playerConnecting', function()
-local name = GetPlayerName(source)
-local ip = GetPlayerEndpoint(source)
+local name = akunbotnub(source)
+local ip = (source)
 local ping = GetPlayerPing(source)
 local steamhex = GetPlayerIdentifier(source)
 local connect = {
